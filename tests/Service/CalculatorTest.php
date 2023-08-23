71d495bacd4b8d0ad5e4e68405ee69e3243de2cb
arrakeen <bryanproinfo@gmail.com>
2023-08-23 09:51:57 +0200

    calculator

---
+++ tests/Service/CalculatorTest.php
@@ -0,0 +1,47 @@
+
+<?php
+
+use App\Service\Calculator;
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CalculatorTest extends KernelTestCase
+{
+    public function testGetTotalHT()
+    {
+        $calculator = new Calculator();
+        $product1 = new Product();
+        $product1->setPrice(5);
+        $product2 = new Product();
+        $product2->setPrice(25);
+
+        $products = [
+            ['product' => $product1, 'quantity' => 4],
+            ['product' => $product2, 'quantity' => 3],
+        ];
+
+        $totalHT = $calculator->getTotalHT($products);
+
+        $this->assertEquals(5 * 4 + 25 * 3, $totalHT);
+    }
+
+    public function testGetTotalTTC()
+    {
+        $calculator = new Calculator();
+        $product1 = new Product();
+        $product1->setPrice(5);
+        $product2 = new Product();
+        $product2->setPrice(25);
+
+        $products = [
+            ['product' => $product1, 'quantity' => 4],
+            ['product' => $product2, 'quantity' => 3],
+        ];
+        
+        $tva = 20;
+        
+        $totalTTC = $calculator->getTotalTTC($products, $tva);
+
+        $this->assertEquals((5 * 4 + 25 * 3) * (1 + ($tva / 100)), $totalTTC);
+    }
+}
